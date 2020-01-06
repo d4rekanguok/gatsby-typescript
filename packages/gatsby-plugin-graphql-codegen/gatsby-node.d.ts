a42966b3db2d12b5409317464ab329ca9fb3b7e1
Derek Nguyen <derek@penandpillow.com>
2020-01-06 13:23:15 +0900

    use gatsby-plugin-graphql-codegen

--- packages/gatsby-plugin-graphql-codegen/gatsby-node.d.ts
+++
@@ -1,8 +0,0 @@
-import { GatsbyNode, PluginOptions } from 'gatsby';
-export interface TsOptions extends PluginOptions {
-    documentPaths?: string[];
-    fileName?: string;
-    codegen?: boolean;
-    codegenDelay?: number;
-}
-export declare const onPostBootstrap: GatsbyNode["onPostBootstrap"];
--- packages/gatsby-plugin-ts/src/gatsby-node.ts
+++ packages/gatsby-plugin-ts/src/gatsby-node.ts
@@ -2,7 +2,7 @@ import { GatsbyNode, PluginOptions } from 'gatsby'
 import * as webpack from 'webpack'
 import * as tsloader from 'ts-loader'
 import FTCWebpackPlugin from 'fork-ts-checker-webpack-plugin'
-import { generateWithConfig } from './graphql-codegen.config'
+import { onPostBootstrap as onPostBootstrapCodegen } from 'gatsby-plugin-graphql-codegen/gatsby-node'
 import debounce from 'lodash.debounce'
 
 export interface TsOptions extends PluginOptions {
@@ -75,46 +75,7 @@ const createRule: CreateRule = (jsLoader, { tsLoader }) => ({
   }],
 })
 
-export const onPostBootstrap: GatsbyNode["onPostBootstrap"] = async (
-  { store, reporter }, pluginOptions: TsOptions
-) => {
-  const options = getOptions(pluginOptions)
-  if (!options.codegen) return
-
-  const fileName = options.fileName as string
-  const codegenDelay = options.codegenDelay
-
-  const { schema, program } = store.getState()
-  const { directory } = program
-  const generateFromSchema = await generateWithConfig({
-    directory, fileName, reporter
-  })
-
-  const build = async (schema: any) => {
-    try {
-      await generateFromSchema(schema) 
-      reporter.info(`[gatsby-plugin-ts] definition for queries has been updated at ${fileName}`)
-    } catch (err) {
-      reporter.panic(err)
-    }
-  }
-
-  const buildDebounce = debounce(build, codegenDelay, {
-    trailing: true,
-    leading: false,
-  })
-
-  const watchStore = async () => {
-    const { lastAction: action } = store.getState()
-    if (!['REPLACE_STATIC_QUERY', 'QUERY_EXTRACTED'].includes(action.type)) return
-    const { schema } = store.getState()
-    await buildDebounce(schema)
-  }
-
-  // HACKY: might break when gatsby updates
-  store.subscribe(watchStore)
-  await build(schema)
-}
+export const onPostBootstrap = onPostBootstrapCodegen
 
 export const onPreInit: GatsbyNode['onPreInit'] = ({ reporter }, options: TsOptions) => {
   const { alwaysCheck } = options
