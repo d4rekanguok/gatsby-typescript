36610639f76ccf2d767bb49fca33b00b5da878f5
Rico Kahler <ricokahler@me.com>
2020-02-09 15:52:16 -0500

    typecheck?

--- packages/gatsby-starter-ts/gatsby-config.js
+++ packages/gatsby-starter-ts/gatsby-config.js
@@ -27,12 +27,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-ts`,
-      options: {
-        typeCheck: process.env.NODE_ENV === 'development'
-      }
-    }
+    `gatsby-plugin-ts`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
