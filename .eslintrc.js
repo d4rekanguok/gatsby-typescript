4caac1bfbf10ed1252064a51e4919079a913ca61
Derek Nguyen <derek@penandpillow.com>
2020-02-13 03:57:58 +0900

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,7 +42,7 @@ module.exports = {
       },
       extends: [
         "plugin:jest/recommended"
-      ]
+      ],
       parserOptions: {
         ecmaVersion: 2019,
         sourceType: "module",
