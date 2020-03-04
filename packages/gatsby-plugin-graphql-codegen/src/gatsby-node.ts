07abadcaad4a812bd14cc990a459c7fe460390e8
Kim Jeker <git@kije.ch>
2020-03-04 10:01:12 +0100

    added possibility to define additional schemas for which types should be generated

--- packages/gatsby-plugin-graphql-codegen/src/gatsby-node.ts
+++ packages/gatsby-plugin-graphql-codegen/src/gatsby-node.ts
@@ -2,6 +2,21 @@ import { GatsbyNode, PluginOptions } from 'gatsby'
 import { generateWithConfig } from './graphql-codegen.config'
 import debounce from 'lodash.debounce'
 import { GraphQLTagPluckOptions } from '@graphql-toolkit/graphql-tag-pluck'
+import { loadSchema, UnnormalizedTypeDefPointer } from '@graphql-toolkit/core'
+import { UrlLoader } from '@graphql-toolkit/url-loader'
+import { JsonFileLoader } from '@graphql-toolkit/json-file-loader'
+import { GraphQLFileLoader } from '@graphql-toolkit/graphql-file-loader'
+import { GraphQLSchema } from 'graphql'
+
+export const DEFAULT_SCHEMA_KEY = 'default-gatsby-schema'
+
+export interface SchemaConfig {
+  key: string
+  fileName: string
+  schema: UnnormalizedTypeDefPointer
+  documentPaths?: string[]
+  pluckConfig?: GraphQLTagPluckOptions
+}
 
 export interface TsOptions extends PluginOptions {
   documentPaths?: string[]
@@ -9,6 +24,7 @@ export interface TsOptions extends PluginOptions {
   codegen?: boolean
   codegenDelay?: number
   pluckConfig?: GraphQLTagPluckOptions
+  additionalSchemas?: SchemaConfig[]
 }
 
 const defaultOptions: Required<TsOptions> = {
@@ -30,6 +46,7 @@ const defaultOptions: Required<TsOptions> = {
       },
     ],
   },
+  additionalSchemas: [],
 }
 
 type GetOptions = (options: TsOptions) => Required<TsOptions>
@@ -37,6 +54,33 @@ const getOptions: GetOptions = pluginOptions => ({
   ...defaultOptions,
   ...pluginOptions,
 })
+type LoadAdditionalSchema = (
+  schema: UnnormalizedTypeDefPointer
+) => Promise<GraphQLSchema>
+const loadAdditionalSchema: LoadAdditionalSchema = schema =>
+  loadSchema(schema, {
+    loaders: [new UrlLoader(), new JsonFileLoader(), new GraphQLFileLoader()],
+  })
+
+type PreparedSchemas = { [key: string]: GraphQLSchema }
+type PrepareSchemas = (
+  defaultSchema: GraphQLSchema,
+  additionalSchemaConfigs: SchemaConfig[]
+) => Promise<PreparedSchemas>
+const prepareSchemas: PrepareSchemas = async (
+  defaultSchema,
+  additionalSchemaConfigs
+) => {
+  const additionalSchemas: PreparedSchemas = {}
+  for (const config of additionalSchemaConfigs) {
+    additionalSchemas[config.key] = await loadAdditionalSchema(config.schema)
+  }
+
+  return {
+    [DEFAULT_SCHEMA_KEY]: defaultSchema,
+    ...additionalSchemas,
+  }
+}
 
 export const onPostBootstrap: NonNullable<GatsbyNode['onPostBootstrap']> = async (
   { store, reporter },
@@ -45,24 +89,63 @@ export const onPostBootstrap: NonNullable<GatsbyNode['onPostBootstrap']> = async
   const options = getOptions(pluginOptions)
   if (!options.codegen) return
 
-  const { documentPaths, fileName, codegenDelay, pluckConfig } = options
-
-  const { schema, program } = store.getState()
-  const { directory } = program
-  const generateFromSchema = await generateWithConfig({
+  const {
     documentPaths,
-    directory,
     fileName,
-    reporter,
+    codegenDelay,
     pluckConfig,
-  })
+    additionalSchemas,
+  } = options
 
-  const build = async (schema: any): Promise<void> => {
+  const {
+    schema,
+    program,
+  }: { schema: GraphQLSchema; program: any } = store.getState()
+  const { directory } = program
+
+  const codegenConfigs = [
+    {
+      key: DEFAULT_SCHEMA_KEY,
+      fileName,
+      documentPaths,
+      pluckConfig,
+      directory,
+      reporter,
+    },
+    ...additionalSchemas.map(({ schema, ...rest }) => {
+      return {
+        documentPaths,
+        pluckConfig,
+        directory,
+        reporter,
+        ...rest,
+      }
+    }),
+  ]
+
+  const formSchemaGenerators = await Promise.all(
+    codegenConfigs.map(async ({ key, ...initialConfig }) => ({
+      key,
+      generateFromSchema: await generateWithConfig(initialConfig),
+    }))
+  )
+
+  const build = async (schemas: PreparedSchemas): Promise<void> => {
+    console.dir(schemas)
     try {
-      await generateFromSchema(schema)
-      reporter.info(
-        `[gatsby-plugin-graphql-codegen] definition for queries has been updated at ${fileName}`
-      )
+      for (const { key, generateFromSchema } of formSchemaGenerators) {
+        const schema = schemas[key]
+        if (!schema) {
+          reporter.panic(
+            `[gatsby-plugin-graphql-codegen] schema ${key} does not exist!`
+          )
+        }
+
+        await generateFromSchema(schema)
+        reporter.info(
+          `[gatsby-plugin-graphql-codegen] definition for queries of schema ${key} has been updated at ${fileName}`
+        )
+      }
     } catch (err) {
       reporter.panic(err)
     }
@@ -79,10 +162,10 @@ export const onPostBootstrap: NonNullable<GatsbyNode['onPostBootstrap']> = async
       return
     }
     const { schema } = store.getState()
-    await buildDebounce(schema)
+    await buildDebounce(await prepareSchemas(schema, additionalSchemas))
   }
 
   // HACKY: might break when gatsby updates
   store.subscribe(watchStore)
-  await build(schema)
+  await build(await prepareSchemas(schema, additionalSchemas))
 }
